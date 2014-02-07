40b720b9c4d9a7072f3bc065a62e20db68dcbf25
zxy844288792 <zhou326@purdue.edu>
2014-02-07 14:56:22 -0500

    ss

---
+++ 368/sorting_main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+long *Load_File(char *Filename, int *Size);
+int Save_File(char *Filename, long *Array, int Size);
+void Shell_Insertion_Sort(long *Array, int Size, double *N_Comp, double *N_Move);
+void Shell_Selection_Sort(long *Array, int Size, double *N_Comp, double *N_Move);
+
+int main()
+{
+  char *filename;
+  int *Size;
+  double *N_Comp;
+  double *N_Move;
+  long * Array = load_File(Filename,Size);
+  
+}
